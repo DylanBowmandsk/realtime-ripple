71eaa6a6437df5771d1d4263bb86e3cf3344c5d7
Dylan <dylanbowmandsk@gmail.com>
2022-01-24 04:41:43 +0000

    added firebase to project

--- src/Messages.jsx
+++ src/Messages.jsx
@@ -1,7 +1,6 @@
 const Messages = ({messages}) => {
     return(
         <div>
-            {console.log(messages)}
             {messages && messages.map((messages, key) => {
                 return(
                     <div key={key}>
